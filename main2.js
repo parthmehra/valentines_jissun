b55611826096ccf7a81f20682547f588d10080c3
parthmehra <parthmehra18@gmail.com>
2026-02-02 15:41:24 +0530

    minor cringe changes

--- main2.js
+++ main2.js
@@ -102,13 +102,13 @@ document.addEventListener("DOMContentLoaded", function () {
         const wedate = document.getElementById("wedate");
         const btns = document.getElementById("btns");
         btns.style.display = "none";
-        wedate.innerText = "We are each other's valentine now. I love you cutie. ❤️😘";
+        wedate.innerText = "We are each other's valentine now. I love you babeee. ❤️";
 
         // Open index1.html in a new tab
         window.open("index1.html", "_blank");
 
     } else {
-        alert("Don't say yes right away, cutie. Play around a bit 😉😘");
+        alert("Don't say yes right away. Play around a bit 😉😘");
     }
 }
 
